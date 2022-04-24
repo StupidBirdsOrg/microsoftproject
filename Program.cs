d14e87118c500dd579e356db1feea8119fe9e242
姚 华 <370933147@qq.com>
2022-04-24 21:47:14 +0800

    swagger增加注释

--- Program.cs
+++ Program.cs
@@ -11,7 +11,13 @@ builder.Services
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // using System.Reflection;
+    var xmlFilename = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+}
+);
 
 
 
